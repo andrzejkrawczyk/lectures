9b81232baf8d57d526cf659b2ac69d18f718c3d3
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-11-17 16:15:11 +0100

    Dodane opisy do przykładów

--- biu/example-projects/InputTestingExamples/src/app/app.component.ts
+++ biu/example-projects/InputTestingExamples/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    currentScreen = 2;
+    currentScreen = 1;
     changeScreen(i: number) {
         this.currentScreen = i;
     }
