7f3e8891c09bac4dc7469866fe2704bde40503f3
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-11-21 16:33:35 +0100

    Spłaszczenie struktury katalogów

--- biu/example-projects/InputTestingExamples/src/app/app.module.ts
+++ biu/example-projects/InputTestingExamples/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PushComponent } from './input/controllers/Standardowe/push/push.component';
-import { SetNewArrayComponent } from './input/controllers/Set/set-new-array/set-new-array.component';
-import { SetReferenceComponent } from './input/controllers/Set/set-reference/set-reference.component';
-import { EvenPipe } from './input/pipes/even.pipe';
-import { PipeComponent } from './input/controllers/Pipe/pipe/pipe.component';
+import { PushComponent } from './components/Standardowe/push/push.component';
+import { SetNewArrayComponent } from './components/Set/set-new-array/set-new-array.component';
+import { SetReferenceComponent } from './components/Set/set-reference/set-reference.component';
+import { EvenPipe } from './pipes/even.pipe';
+import { PipeComponent } from './components/Pipe/pipe/pipe.component';
 
 @NgModule({
     declarations: [
