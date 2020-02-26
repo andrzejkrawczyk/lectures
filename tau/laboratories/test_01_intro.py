a08323853db6460335ad8bad163021c16b8c8046
Andrzej Krawczyk <andrzej.krawczyk@solwit.com>
2020-02-27 00:10:16 +0100

    [TAU] Pytest intro

---
+++ tau/laboratories/test_01_intro.py
@@ -0,0 +1,9 @@
+from josephus import josephus
+
+def test_josephus():
+    user_input = list(range(1, 10))
+    expected_output = [3, 6, 9, 4, 8, 5, 2, 7, 1]
+    
+    result = list(josephus(user_input, 3))
+    
+    assert result == expected_output
